796b3e649a84e6d62c57f92c1c0ea70c869f83cf
emanuel06lm <emanuel06lm@gmail.com>
2018-10-25 08:30:45 -0500

    Init commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+import com.cclass.frames.MainScreen;
+
+public class Main{
+
+	public Main(){
+		
+
+	}
+
+	public static void main(String[] args) {
+		MainScreen frame = new MainScreen();
+	}
+}
---
+++ com/cclass/frames/MainScreen.java
@@ -0,0 +1,16 @@
+package com.cclass.frames;
+import javax.swing.*;
+
+
+
+public class MainScreen extends JFrame{
+
+	public MainScreen(){
+		setVisible(true);
+		setSize(800,600);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+
+}
+
